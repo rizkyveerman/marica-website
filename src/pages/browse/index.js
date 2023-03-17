d8f72d777ea271e3615cf9d4f4cc36f62ab7b984
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-17 07:02:05 +0700

    configure public apis for development testing

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -3,21 +3,9 @@ import MainLayout from "@/layout/MainLayout";
 import React from "react";
 
 export async function getServerSideProps(ctx) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "67bab0cae7msh55022c298b7ebc4p12216bjsnbe8224d9d264",
-      "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
-    },
-  };
-
   const movies = await fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => response)
-    .catch((error) => console.log("error:: ", error));
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en"
+  ).then((response) => response.json());
 
   return {
     props: {
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,28 +1,40 @@
 import Link from "next/link";
 import React from "react";
 
-const Profile = () => {
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const users = await res.json();
+
+  return {
+    props: { users },
+  };
+}
+
+const Profile = ({ users }) => {
   return (
     <article className="bg-abu-terang">
       <section className="w-full min-h-screen p-4 grid place-content-center">
         <h2 className="mb-8">Siapa yang menonton?</h2>
         <div className="flex justify-center gap-4">
-          <div>
-            <div className="h-20 w-20 bg-mangga rounded-lg"></div>
-            <p className="">Imam</p>
-          </div>
-          <div>
-            <div className="h-20 w-20 bg-mangga rounded-lg"></div>
-            <p className="">Joni</p>
-          </div>
-          <div>
-            <Link
-              href="/add-kids"
-              className="h-20 w-20 border grid place-content-center border-mangga rounded-lg text-2xl"
-            >
-              +
-            </Link>
-          </div>
+          <ul className="grid grid-cols-4 gap-4">
+            {users.map((user) => (
+              <li key={user.id}>
+                <div>
+                  <div className="h-20 w-20 bg-mangga rounded-lg"></div>
+                  <p className="">{user.name}</p>
+                </div>
+              </li>
+            ))}
+            <div>
+              <Link
+                href="/add-kids"
+                className="h-20 w-20 border grid place-content-center border-mangga rounded-lg text-2xl"
+              >
+                +
+              </Link>
+            </div>
+          </ul>
         </div>
       </section>
     </article>
