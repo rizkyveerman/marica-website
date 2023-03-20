6200b51b6a00df36a02800539a96a76c312ae74e
Rizky <rizfirsy@gmail.com>
2023-03-20 08:54:41 +0700

    implementing uesr session

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
