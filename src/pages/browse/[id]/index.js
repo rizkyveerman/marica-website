7c23ca08660ebbbbc76131365bdd062a4d547bdf
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-20 11:51:49 +0700

    debugging

--- src/pages/browse/[id]/index.js
+++ src/pages/browse/[id]/index.js
@@ -5,36 +5,35 @@ import { useRouter } from "next/router";
 
 const MoviesDetails = ({ movie }) => {
   const router = useRouter();
-  console.log("movie: ", movie);
 
   const { slug } = router;
 
   return <div>MoviesDetails: {slug}</div>;
 };
 
-export async function getStaticPaths() {
-  const movies = await loadMovies();
+// export async function getStaticPaths() {
+//   const movies = await loadMovies();
 
-  const moviesPaths = movies.map((movie) => {
-    return {
-      params: {
-        id: movie.id,
-      },
-    };
-  });
+//   const moviesPaths = movies.map((movie) => {
+//     return {
+//       params: {
+//         id: movie.id.toString(),
+//       },
+//     };
+//   });
 
-  console.log("moviesPaths", moviesPaths);
+//   console.log("moviesPaths", moviesPaths);
 
-  return { paths: moviesPaths, fallback: false };
-}
+//   return { paths: moviesPaths, fallback: false };
+// }
 
-export async function getStaticProps(ctx) {
-  const id = ctx.params.id;
-  const movie = loadMovie(id);
+// export async function getStaticProps(ctx) {
+//   const id = ctx.params.id;
+//   const movie = loadMovie(id);
 
-  return {
-    props: { movie },
-  };
-}
+//   return {
+//     props: { movie },
+//   };
+// }
 
 export default MoviesDetails;
