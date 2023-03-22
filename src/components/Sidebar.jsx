61dc5c64b39f4912da9c7371d433a0bc6a60e14a
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-22 20:46:27 +0700

    change sidebar color scheme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 import Logo from "./Logo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faMagnifyingGlass,
+  faHome,
   faComments,
   faFilm,
   faBookmark,
   faGear,
   faQuestion,
+  faCircleInfo,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
@@ -17,11 +18,30 @@ const Sidebar = () => {
   console.log("router", router.route);
 
   return (
-    <aside className="w-full h-full">
+    <aside className="relative w-full h-full before:absolute">
       <div className="w-full h-full flex flex-col justify-between">
         <ul className="p-4 w-full fixed left-0 right-0 bottom-0 md:relative h-24 md:h-0 flex justify-center md:justify-start item-start lg:grid grid-cols-1 md:border-0 gap-8 md:gap-1">
           <li className="hidden lg:block">
             <Logo />
+          </li>
+          <li
+            className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
+              router.route === "/"
+                ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
+                : "text-arang/80"
+            } rounded-xl lg:pl-4`}
+          >
+            {/* <Image
+                src={activity}
+                alt="activity"
+                width={18}
+                height={18}
+                className="w-8 md:w-[18px]"
+              /> */}
+            <FontAwesomeIcon icon={faHome} height={20} />
+            <Link className="hidden md:block py-4 px-4 w-full" href="/">
+              Home
+            </Link>
           </li>
           <li
             className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
@@ -91,7 +111,7 @@ const Sidebar = () => {
             </Link>
           </li>
         </ul>
-        <ul className="bg-gradient-to-tr from-pink-200 to-pink-100 hidden lg:grid grid-cols-1 gap-1 p-4 border-t-2 border-pink-200">
+        <ul className="hidden lg:grid grid-cols-1 gap-1 p-4">
           <li
             className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
               router.route === "/about"
@@ -99,7 +119,7 @@ const Sidebar = () => {
                 : "text-arang/80"
             } rounded-xl lg:pl-4`}
           >
-            <FontAwesomeIcon icon={faQuestion} height={20} />
+            <FontAwesomeIcon icon={faCircleInfo} height={20} />
             <Link className="hidden md:block py-4 px-4 w-full" href="/about">
               Tentang Marica
             </Link>
