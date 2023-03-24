4c652eaa97f32edc70913d0f0994b5f0ba7b198d
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-25 05:19:28 +0700

    redesigning sidebar

--- src/widgets/Sidebar.jsx
+++ src/widgets/Sidebar.jsx
@@ -4,67 +4,119 @@ import Link from "next/link";
 import Logo from "@/components/Logo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faHome,
-  faComments,
+  faArrowUpRightDots,
   faFilm,
-  faBookmark,
+  faTableCells,
+  faListOl,
+  faHeart,
+  faComments,
   faGear,
   faQuestion,
-  faCircleInfo,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
+
+const menus = [
+  {
+    name: "Semua film",
+    icon: faFilm,
+    path: "all-movies",
+  },
+  {
+    name: "Trending",
+    icon: faArrowUpRightDots,
+    path: "trending",
+  },
+  {
+    name: "Kategori",
+    icon: faTableCells,
+    path: "categories",
+  },
+];
+const libraries = [
+  {
+    name: "Aktifitas",
+    icon: faComments,
+    path: "activity",
+  },
+  {
+    name: "Playlist",
+    icon: faListOl,
+    path: "playlist",
+  },
+  {
+    name: "Disukai",
+    icon: faHeart,
+    path: "likes",
+  },
+];
 
 const Sidebar = () => {
   const router = useRouter();
 
   return (
-    <aside className="h-full">
-      <div className="h-full flex flex-col justify-between">
-        <ul className="relative left-0 right-0 bottom-0 border p-4 flex justify-between item-center gap-1 lg:grid grid-cols-1 md:border-0">
-          <li className="hidden lg:block">
-            <Logo />
-          </li>
-          <li
-            className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
-              router.route === "/"
-                ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
-                : "text-arang/80"
-            } rounded-xl lg:pl-4`}
-          >
-            {/* <Image
-                src={activity}
-                alt="activity"
-                width={18}
-                height={18}
-                className="w-8 md:w-[18px]"
-              /> */}
-
-            <Link className="flex gap-2 p-4 lg:py-4 lg:px-0 w-full" href="/">
-              <FontAwesomeIcon icon={faHome} height={20} />
-              <span className="hidden md:block">Home</span>
-            </Link>
-          </li>
-          <li
-            className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
-              router.route === "/activities"
-                ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
-                : "text-arang/80"
-            } rounded-xl lg:pl-4`}
-          >
-            {/* <Image
-                src={activity}
-                alt="activity"
-                width={18}
-                height={18}
-                className="w-8 md:w-[18px]"
-              /> */}
-            <Link
-              className="flex gap-2 p-4 lg:py-4 lg:px-0 w-full"
-              href="/activities"
+    <aside className="w-full h-full">
+      <div className="w-full h-full flex lg:flex-col justify-center lg:justify-between items-center lg:items-start">
+        <div className="w-full flex lg:flex-col">
+          <ul className="w-full relative p-4 flex justify-between item-center gap-1 lg:grid grid-cols-1 ">
+            <li>
+              <p className="hidden md:block text-xs uppercase tracking-widest">
+                menu
+              </p>
+            </li>
+            {menus.map((menu, index) => (
+              <li
+                key={index}
+                className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
+                  router.route === "/all-movies"
+                    ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
+                    : "text-arang/80"
+                } rounded-xl lg:pl-4`}
+              >
+                <Link
+                  className="flex items-center gap-2 p-4 lg:py-4 lg:px-0 w-full"
+                  href={`/${menu.path}`}
+                >
+                  <FontAwesomeIcon icon={menu.icon} height={14} />
+                  <span className="hidden md:block">{menu.name}</span>
+                </Link>
+              </li>
+            ))}
+            <li
+              className={`flex justify-start items-center hover:bg-pink-100 bg-gradient-to-t hover:text-pink-600 text-arang/80 rounded-xl lg:pl-4`}
             >
-              <FontAwesomeIcon icon={faComments} height={20} />
-              <span className="hidden md:block">Activities</span>
-            </Link>
-          </li>
+              <button className="flex items-center gap-2 p-4 lg:py-4 lg:px-0 w-full">
+                <FontAwesomeIcon icon={faQuestion} height={14} />
+                <span className="hidden md:block">Menu</span>
+              </button>
+            </li>
+          </ul>
+          <ul className="hidden w-full relative bottom-0 p-4 gap-1 lg:grid grid-cols-1 ">
+            <li>
+              <p className="hidden md:block text-xs uppercase tracking-widest">
+                libraries
+              </p>
+            </li>
+            {libraries.map((menu, index) => (
+              <li
+                key={index}
+                className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
+                  router.route === "/all-movies"
+                    ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
+                    : "text-arang/80"
+                } rounded-xl lg:pl-4`}
+              >
+                <Link
+                  className="flex items-center gap-2 p-4 lg:py-4 lg:px-0 w-full"
+                  href={`/${menu.path}`}
+                >
+                  <FontAwesomeIcon icon={menu.icon} height={14} />
+                  <span className="hidden md:block">{menu.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className="hidden p-4 lg:grid grid-cols-1">
           <li
             className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
               router.route === "/all-movies"
@@ -73,57 +125,10 @@ const Sidebar = () => {
             } rounded-xl lg:pl-4`}
           >
             <Link
-              className="flex gap-2 p-4 lg:py-4 lg:px-0 w-full"
-              href="/all-movies"
-            >
-              <FontAwesomeIcon icon={faFilm} height={20} />
-              <span className="hidden md:block">Semua movies</span>
-            </Link>
-          </li>
-          <li
-            className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
-              router.route === "/watchlist"
-                ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
-                : "text-arang/80"
-            } rounded-xl lg:pl-4`}
-          >
-            <Link
-              className="flex gap-2 p-4 lg:py-4 lg:px-0 w-full"
-              href="/watchlist"
-            >
-              <FontAwesomeIcon icon={faBookmark} height={20} />
-              <span className="hidden md:block">Watchlist</span>
-            </Link>
-          </li>
-        </ul>
-        <ul className="hidden lg:grid grid-cols-1 gap-1 p-4">
-          <li
-            className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
-              router.route === "/about"
-                ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
-                : "text-arang/80"
-            } rounded-xl lg:pl-4`}
-          >
-            <Link
-              className="flex gap-2 p-4 lg:py-4 lg:px-0 w-full"
-              href="/about"
-            >
-              <FontAwesomeIcon icon={faCircleInfo} height={20} />
-              <span className="hidden md:block">Tentang Marica</span>
-            </Link>
-          </li>
-          <li
-            className={`flex justify-start items-center hover:bg-pink-100 hover:text-semangka ${
-              router.route === "/help"
-                ? "bg-gradient-to-t text-white hover:bg-gradient-to-t from-pink-600 to-pink-300 hover:text-white"
-                : "text-arang/80"
-            } rounded-xl lg:pl-4`}
-          >
-            <Link
-              className="flex gap-2 p-4 lg:py-4 lg:px-0 w-full"
+              className="flex items-center gap-2 p-4 lg:py-4 lg:px-0 w-full"
               href="/help"
             >
-              <FontAwesomeIcon icon={faQuestion} height={20} />
+              <FontAwesomeIcon icon={faQuestion} height={14} />
               <span className="hidden md:block">Help</span>
             </Link>
           </li>
