ed9d61e2bf2ba50d81d69ce8613e44ead891bde6
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-24 09:11:43 +0700

    refactoring login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-import background from "@/images/background.jpg";
+import { ErrorMessage, Formik, Form, Field } from "formik";
 import Button from "@/components/buttons/Button";
 import Link from "next/link";
 import Footer from "@/widgets/Footer";
@@ -41,7 +41,62 @@ function LoginPage() {
               Masuk ke akun Marica kamu dan nikmati berbagai kategori film dan
               series yang kamu suka!
             </p>
-            <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                  errors.email = "required";
+                } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                  errors.email = "Invalid email address";
+                }
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form className="grid grid-cols-1 gap-4 w-full">
+                  <div className="email w-full relative grid gap-2">
+                    <p>Masukkan email</p>
+                    <Field
+                      type="email"
+                      name="email"
+                      className="w-full py-3 px-6 rounded-lg bg-abu-terang"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="m-auto absolute top-full p-2 rounded-xl bg-red-100 text-red-600 before:block before:absolute before:left-1/2 before:bottom-full before:w-3 before:h-3 before:bg-red-100 before:translate-y-1/2 before:-translate-x-1/2 before:rotate-45 before:rounded-sm z-10"
+                    />
+                  </div>
+                  <div className="password w-full relative grid gap-2">
+                    <p>Masukkan password</p>
+                    <Field
+                      type="password"
+                      name="password"
+                      className="py-3 px-6 rounded-lg bg-abu-terang"
+                    />
+                    <ErrorMessage name="password" component="div" />
+                  </div>
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    disabled={isSubmitting}
+                    isClicked={() => "clicked"}
+                  >
+                    Masuk
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+            {/* <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
               <Input
                 placeholder="Masukkan email"
                 ref={usernameRef}
@@ -78,7 +133,7 @@ function LoginPage() {
                 </Link>
               </div>
             
-            </form>
+            </form> */}
             <p className="text-sm text-abu text-center">
               Belum punya akun Marica?{" "}
               <Link href="/create-account" className="text-pink-600 underline">
