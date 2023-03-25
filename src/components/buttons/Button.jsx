68398279a645054b76ec13d26ed08cdb8d13e6ce
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-25 23:43:07 +0700

    [modify] button padding

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -20,7 +20,7 @@ const Button = ({
         variant === "primary"
           ? "bg-gradient-to-t from-pink-600 to-pink-400 text-white drop-shadow-md hover:opacity-75"
           : "bg-transparent text-slate-600 border border-slate-600 hover:border-pink-600 hover:text-pink-600"
-      } flex justify-center items-center gap-1 px-5 py-3 rounded-xl`}
+      } flex justify-center items-center gap-1 px-5 py-2 rounded-xl`}
       onClick={isLink ? null : () => isClicked()}
     >
       {icon}
