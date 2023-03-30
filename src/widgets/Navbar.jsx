c4865598bd13f14439ebda208ff6781299312ded
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-30 20:43:50 +0700

    consuming user info

--- src/widgets/Navbar.jsx
+++ src/widgets/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = () => {
                 className="rounded-full bg-pink-600"
               />
               <p className="text-slate-600 hidden lg:flex items-center gap-2">
-                <span>{userInfo.nama}</span>
+                <span>{userInfo.data.nama}</span>
                 <FontAwesomeIcon icon={faCaretDown} height={14} />
               </p>
             </div>
