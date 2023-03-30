c9f66fa9c4c1e5372e03a8420e660bb6a8ed7a79
Rizky Firman Syah <rizfirsy@gmail.com>
2023-03-30 20:05:31 +0700

    hide special navbar content whenever no userinfo

--- src/widgets/Navbar.jsx
+++ src/widgets/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "@/store/slices/theme";
+import Button from "@/components/buttons/Button";
 
 import {
   faCaretDown,
@@ -12,7 +13,6 @@ import Logo from "../components/Logo";
 import Link from "next/link";
 import Image from "next/image";
 import woman from "@/icons/woman.png";
-import Button from "@/components/buttons/Button";
 import lightmode from "@/images/lightmode.png";
 import darkmode from "@/images/darkmode.png";
 
@@ -20,6 +20,7 @@ const Navbar = () => {
   const [profileMenu, setProfileMenu] = useState(false);
   const dispatch = useDispatch();
   const appTheme = useSelector((state) => state.theme.theme);
+  const { isLoading, error, userInfo } = useSelector((state) => state.user);
 
   return (
     <header className="w-full relative p-4 flex justify-between items-center bg-white z-50">
@@ -75,37 +76,43 @@ const Navbar = () => {
           </div>
         )}
         <div className="w-full flex gap-2 md:gap-4 justify-end items-center">
-          <Link
-            href="/subscription"
-            className="text-pink-700 flex justify-start items-center gap-1 w-fit py-1 px-2 rounded-xl bg-gradient-to-t from-pink-400 to-pink-200 text-xs border-2 border-pink-300 hover:text-white"
-          >
-            <FontAwesomeIcon icon={faCrown} height={14} />
-            <p className="text-white">Upgrade ke Pro</p>
-          </Link>
-          <div className="hover:text-pink-600 text-slate-600 cursor-pointer">
-            <FontAwesomeIcon icon={faBell} height={20} />
-          </div>
-          <div
-            className="flex items-center gap-2 cursor-pointer"
-            onClick={() => setProfileMenu(!profileMenu)}
-          >
-            <Image
-              src={woman}
-              alt="Rica"
-              width={32}
-              height={32}
-              className="rounded-full bg-pink-600"
-            />
-            <p className="text-slate-600 hidden lg:flex items-center gap-2">
-              <span>Maria</span>
-              <FontAwesomeIcon icon={faCaretDown} height={14} />
-            </p>
-          </div>
+          {userInfo && (
+            <Link
+              href="/subscription"
+              className="text-pink-700 flex justify-start items-center gap-1 w-fit py-1 px-2 rounded-xl bg-gradient-to-t from-pink-400 to-pink-200 text-xs border-2 border-pink-300 hover:text-white"
+            >
+              <FontAwesomeIcon icon={faCrown} height={14} />
+              <p className="text-white">Upgrade ke Pro</p>
+            </Link>
+          )}
+          {userInfo && (
+            <div className="hover:text-pink-600 text-slate-600 cursor-pointer">
+              <FontAwesomeIcon icon={faBell} height={20} />
+            </div>
+          )}
+          {userInfo ? (
+            <div
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => setProfileMenu(!profileMenu)}
+            >
+              <Image
+                src={woman}
+                alt="Rica"
+                width={32}
+                height={32}
+                className="rounded-full bg-pink-600"
+              />
+              <p className="text-slate-600 hidden lg:flex items-center gap-2">
+                <span>Maria</span>
+                <FontAwesomeIcon icon={faCaretDown} height={14} />
+              </p>
+            </div>
+          ) : (
+            <Button isLink type="button" variant="secondary" href="/login">
+              Masuk ke Marica
+            </Button>
+          )}
         </div>
-        {/* is login? */}
-        {/* <Button isLink type="button" variant="secondary" href="/login">
-          Masuk ke Marica
-        </Button> */}
       </div>
     </header>
   );
