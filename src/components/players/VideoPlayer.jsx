1a4e784d70dbd1df8448f9945e17dd3540b5f583
Rizky Firman Syah <rizfirsy@gmail.com>
2023-04-02 13:26:40 +0700

    create video player component

---
+++ src/components/players/VideoPlayer.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const VideoPlayer = ({ src }) => {
+  return (
+    <div className="video-player w-full border border-red-600 ">
+      <video className="border border-blue-500 w-full" src={src} autoPlay />
+    </div>
+  );
+};
+
+export default VideoPlayer;
--- src/pages/all-movies/[id]/index.js
+++ src/pages/all-movies/[id]/index.js
@@ -14,11 +14,13 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import Button from "@/components/buttons/Button";
+import VideoPlayer from "@/components/players/VideoPlayer";
 
 const MoviesDetails = ({ movie }) => {
   const [isHoveringEps, setIsHoveringEps] = useState(false);
   return (
     <articles>
+      {/* <VideoPlayer src="/videos/video.mp4" /> */}
       <section className="p-4 h-[60vh]">
         <div className="w-full h-full relative overflow-hidden rounded-xl">
           <div className="absolute left-0 right-0 top-0 bottom-0 z-50 p-4 md:p-8 lg:p-12 flex flex-col justify-end">
