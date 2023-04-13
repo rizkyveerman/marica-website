264c13f6a48a30ba024b2453a2c32c5e633d5480
Rizky Firman Syah <rizfirsy@gmail.com>
2023-04-13 14:06:13 +0700

    debugging: profile page

--- src/components/inputs/Input.jsx
+++ src/components/inputs/Input.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-import propTypes from "prop-types";
+import { Field, ErrorMessage } from "formik";
 
 const Input = ({ ref, label, type, name, onChange, placeholder }) => {
   return (
-    <div>
-      <label htmlFor={name} className="text-arang">
-        {label}
-      </label>
-      <input
-        placeholder={placeholder}
-        ref={ref}
-        id={name}
+    <div className="w-full relative grid gap-2">
+      <label htmlFor={name}>{label}</label>
+      <Field
         type={type}
         name={name}
-        onChange={(e) => onChange(e.target.value)}
-        className="px-5 py-3 bg-transparent rounded-xl border border-arang/30 placeholder:text-sm placeholder:text-abu focus:outline outline-sky-600 focus:border-sky-600 focus:text-sky-600"
+        className="focus:text-sky-600 focus:outline-2 text-slate-700 focus:outline-sky-600 w-full py-3 px-6 rounded-lg bg-abu-terang"
+      />
+      <ErrorMessage
+        name={name}
+        component="div"
+        className="m-auto absolute top-full p-2 rounded-xl bg-red-100 text-red-600 before:block before:absolute before:left-1/2 before:bottom-full before:w-3 before:h-3 before:bg-red-100 before:translate-y-1/2 before:-translate-x-1/2 before:rotate-45 before:rounded-sm z-10"
       />
     </div>
   );
-};
-
-Input.propTypes = {
-  type: propTypes.oneOf(["text", "email", "password"]),
 };
 
 export default Input;
